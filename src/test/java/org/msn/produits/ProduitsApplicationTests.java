2d4f731a351a396ad047cd43008f4b3fcaee83c3
MalicknND <ndiayemalick062@gmail.com>
2026-04-25 15:49:03 +0200

    add test for creating a Produit entity in ProduitsApplicationTests

--- src/test/java/org/msn/produits/ProduitsApplicationTests.java
+++ src/test/java/org/msn/produits/ProduitsApplicationTests.java
@@ -1,13 +1,20 @@
 package org.msn.produits;
 
 import org.junit.jupiter.api.Test;
+import org.msn.produits.entities.Produit;
+import org.msn.produits.repos.ProduitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProduitsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private ProduitRepository produitRepository;
 
+    @Test
+    public void testCreateProduit() {
+        Produit prod = new Produit("MacBook Pro", 2500.0, new java.util.Date());
+        produitRepository.save(prod);
+    }
 }
