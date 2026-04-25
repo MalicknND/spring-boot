a26d94e6fccde4f82aee48b6b463e2dfe9bfc2c0
MalicknND <ndiayemalick062@gmail.com>
2026-04-25 16:14:30 +0200

    test : add tests for finding, updating, deleting, and retrieving all Produit entities in ProduitsApplicationTests

--- src/test/java/org/msn/produits/ProduitsApplicationTests.java
+++ src/test/java/org/msn/produits/ProduitsApplicationTests.java
@@ -6,6 +6,8 @@ import org.msn.produits.repos.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class ProduitsApplicationTests {
 
@@ -14,7 +16,33 @@ class ProduitsApplicationTests {
 
     @Test
     public void testCreateProduit() {
-        Produit prod = new Produit("MacBook Pro", 2500.0, new java.util.Date());
+        Produit prod = new Produit("MacBook Air", 2000.0, new java.util.Date());
         produitRepository.save(prod);
+    }
+
+    @Test
+    public void testFindProduit(){
+        Produit prod = produitRepository.findById(1L).get();
+        System.out.println(prod);
+    }
+
+    @Test
+    public void testUpdateProduit(){
+        Produit prod = produitRepository.findById(1L).get();
+        prod.setNomProduit("MacBook Pro M3");
+        produitRepository.save(prod);
+    }
+
+    @Test
+    public void testDeleteProduit(){
+        produitRepository.deleteById(1L);
+    }
+
+    @Test
+    public void testFindAllProduits(){
+        List<Produit> produits = produitRepository.findAll();
+        for (Produit produit : produits) {
+            System.out.println(produit);
+        }
     }
 }
